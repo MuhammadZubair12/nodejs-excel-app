77705df0fe5fb9e37ead37a869cb0571c02df30f
MuhammadZubair12 <zubairmazhar23@gmail.com>
2021-09-16 17:46:42 +0500

    n

--- api/models/sheet.js
+++ api/models/sheet.js
@@ -1,41 +1,48 @@
 
-module.exports = (sequelize, Sequelize) => {
+const Sequelize = require('sequelize');
+const sequelize = require('../../config/database');
 
-    var Sheet = sequelize.define('sheet', {
-        id: {
-            autoIncrement: true,
-            primaryKey: true,
-            type: Sequelize.INTEGER,
-        },
-        name: {
-            type: Sequelize.STRING,
-        },
-        code: {
-            type: Sequelize.STRING,
-        },
-        category: {
-            type: Sequelize.STRING,
-        },
-        category_code: {
-            type: Sequelize.STRING,
-        },
-        purchase_price: {
-            type: Sequelize.STRING,
-        },
-        sale_price: {
-            type: Sequelize.STRING,
-        },
-        quantity: {
-            type: Sequelize.STRING,
-        },
-        company_id: {
-            type: Sequelize.STRING,
-        },
-        image: {
-            type: Sequelize.STRING,
-        }
+const hooks = {};
 
-    });
+const tableName = 'sheet';
 
-    return Sheet;
-}
+const Sheet = sequelize.define('sheet', {
+    id: {
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+    },
+    name: {
+        type: Sequelize.STRING,
+    },
+    code: {
+        type: Sequelize.STRING,
+    },
+    category: {
+        type: Sequelize.STRING,
+    },
+    category_code: {
+        type: Sequelize.STRING,
+    },
+    purchase_price: {
+        type: Sequelize.STRING,
+    },
+    sale_price: {
+        type: Sequelize.STRING,
+    },
+    quantity: {
+        type: Sequelize.STRING,
+    },
+    company_id: {
+        type: Sequelize.STRING,
+    },
+    image: {
+        type: Sequelize.STRING,
+    }
+}, { hooks, tableName });
+
+
+module.exports = Sheet;
+
+
+
