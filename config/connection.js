a7d68afd583debf9dbe8aef5bcc0572e7ba56252
MuhammadZubair12 <zubairmazhar23@gmail.com>
2021-09-17 19:08:50 +0500

    m

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,16 @@
 const development = {
-  database: 'excellapp',
-  username: 'postgres',
-  password: 'c00822631',
-  host: 'localhost',
+  database: 'dcss0qle32guoj',
+  username: 'zlpxyafjsynnlb',
+  password: '11823af517496d201423e19f7345193a77e3f8140d939e910a7fe124d64aedab',
+  host: 'ec2-34-227-120-94.compute-1.amazonaws.com',
   dialect: 'postgres',
 };
 
 const testing = {
-  database: 'excellapp',
-  username: 'postgres',
-  password: 'c00822631',
-  host: 'localhost',
+  database: 'dcss0qle32guoj',
+  username: 'zlpxyafjsynnlb',
+  password: '11823af517496d201423e19f7345193a77e3f8140d939e910a7fe124d64aedab',
+  host: 'ec2-34-227-120-94.compute-1.amazonaws.com',
   dialect: 'postgres',
 };
 
