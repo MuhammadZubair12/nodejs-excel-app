a46297bbec29130c38abbcf47b6002e90abe9937
MuhammadZubair12 <zubairmazhar23@gmail.com>
2021-09-17 19:37:59 +0500

    f

--- config/connection.js
+++ config/connection.js
@@ -4,6 +4,7 @@ const development = {
   password: '11823af517496d201423e19f7345193a77e3f8140d939e910a7fe124d64aedab',
   host: 'ec2-34-227-120-94.compute-1.amazonaws.com',
   dialect: 'postgres',
+  port:5432,
 };
 
 const testing = {
@@ -12,6 +13,7 @@ const testing = {
   password: '11823af517496d201423e19f7345193a77e3f8140d939e910a7fe124d64aedab',
   host: 'ec2-34-227-120-94.compute-1.amazonaws.com',
   dialect: 'postgres',
+  port:5432,
 };
 
 // const production = {
@@ -28,6 +30,7 @@ const production = {
   password: '11823af517496d201423e19f7345193a77e3f8140d939e910a7fe124d64aedab',
   host: 'ec2-34-227-120-94.compute-1.amazonaws.com',
   dialect: 'postgres',
+  port: 5432,
 };
 
 // const production = {
