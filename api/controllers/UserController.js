78d2070009b5c63e01913786d1757b577fe304c6
MuhammadZubair12 <zubairmazhar23@gmail.com>
2021-09-17 19:33:29 +0500

    f

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -43,26 +43,27 @@ const UserController = () => {
   const sheet = async (req, res) => {
     let sheetdata = [];
     const body = req.body;
-    body.forEach((row) => {
-      let cs =  {
-        name: row[0],
-        code:row[1],
-        category: row[2],
-        category_code: row[3],
-        purchase_price: row[4],
-        sale_price: row[5],
-        quantity: row[6],
-        company_id:row[7],
-        image:row[8]
-    }
-      sheetdata.push(cs)
-    })
-    sheetdata.shift();
-    return await Sheet.bulkCreate(sheetdata).then(_cs=> {
-      return res.status(200).json(_cs);
-    }).catch(err=> {
-      return res.status(500).json(err);
-    });
+    console.log(body)
+    // body.forEach((row) => {
+    //   let cs =  {
+    //     name: row[0],
+    //     code:row[1],
+    //     category: row[2],
+    //     category_code: row[3],
+    //     purchase_price: row[4],
+    //     sale_price: row[5],
+    //     quantity: row[6],
+    //     company_id:row[7],
+    //     image:row[8]
+    // }
+    //   sheetdata.push(cs)
+    // })
+    // sheetdata.shift();
+    // return await Sheet.bulkCreate(sheetdata).then(_cs=> {
+    //   return res.status(200).json(_cs);
+    // }).catch(err=> {
+    //   return res.status(500).json(err);
+    // });
 
   }
 
